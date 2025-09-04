e30998019f5533a23414bad98a49e4b79998a418
AnaBeatriz424 <galdino.silva.ana@escola.pr.gov.br>
2025-09-04 08:28:51 -0300

    ok

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+// Declarando variáveis
+let numero1 = 10;
+let numero2 = 5;
+
+// Operações matemáticas
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+// Exibindo resultados no console
+console.log("número 1:", numero1);
+console.log("número 2:", numero2);
+console.log("soma:", soma);
+console.log("subtração:", subtracao);
+console.log("multiplicação:", multiplicacao);
+console.log("divisão:", divisao);
